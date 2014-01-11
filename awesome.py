46f0ed47c42513c49943f5c7304eeba9ef0ba824
Lafayette Public Library <amelanco@state.lib.la.us>
2014-01-10 19:41:40 -0600

    Update awesome.py

--- awesome.py
+++ awesome.py
@@ -13,10 +13,11 @@ def scancode():
 #    lcd.clear()
 #    lcd.backlight(lcd.VIOLET)
     print("So... You found something awesome?")
-    print("Please Scan Your Awesomeness")
+    print("Please Scan Your Awesomeness:")
 #    lcd.message("Please Scan Your\n")
 #    lcd.message("  Awesome Item")
 #    bcode = str(raw_input())
+    print("Checking for awesomeness...")
     bcode = str(getpass.getpass(prompt=""))  #Get barcode of item wihtout printing it to the screen
     baseurl = ("http://ipac.lafayette.lib.la.us:8080/ipac20/ipac.jsp?term=%s&index=BC" % bcode) #Insert barcode into URL
     html = urlopen(baseurl) #Get HTML code
